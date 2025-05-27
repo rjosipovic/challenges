19c91b0871d6612d32a90ebe8b2240a635775104
Roman Josipović <rjosipovic@gmail.com>
2025-05-27 23:24:11 +0200

    Initialization of notification manager

---
+++ notification-manager/src/main/java/com/playground/notification_manager/NotificationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.playground.notification_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationManagerApplication.class, args);
+	}
+
+}
---
+++ notification-manager/src/test/java/com/playground/notification_manager/NotificationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.notification_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
