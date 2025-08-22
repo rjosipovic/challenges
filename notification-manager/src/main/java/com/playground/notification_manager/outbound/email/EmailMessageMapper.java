711474f59bb653dbf9a50805c2986b930f3cf2be
Roman Josipović <rjosipovic@gmail.com>
2025-08-22 23:54:05 +0200

    Introducing EmailMessageMapperTest
    Modifing and extending EmailSenderTest

--- notification-manager/src/main/java/com/playground/notification_manager/outbound/email/EmailMessageMapper.java
+++ notification-manager/src/main/java/com/playground/notification_manager/outbound/email/EmailMessageMapper.java
@@ -9,21 +9,23 @@ import java.util.Objects;
 public class EmailMessageMapper {
 
     public SimpleMailMessage toSimpleMailMessage(EmailMessage emailMessage) {
-        Objects.requireNonNull(emailMessage, "EmailMessage cannot be null.");
-        String to = emailMessage.getTo();
-        String from = emailMessage.getFrom();
-        String subject = emailMessage.getSubject();
-        String body = emailMessage.getBody();
-
-        if (Objects.isNull(to) || Objects.isNull(from) || Objects.isNull(subject) || Objects.isNull(body)) {
-            throw new IllegalArgumentException("Email 'to', 'from', 'subject', and 'body' cannot be null.");
+        if (isInvalidEmailMessage(emailMessage)) {
+            throw new IllegalArgumentException("EmailMessage and its fields ('to', 'from', 'subject', 'body') must not be null.");
         }
 
         var message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setFrom(from);
-        message.setSubject(subject);
-        message.setText(body);
+        message.setTo(emailMessage.getTo());
+        message.setFrom(emailMessage.getFrom());
+        message.setSubject(emailMessage.getSubject());
+        message.setText(emailMessage.getBody());
         return message;
+    }
+
+    private boolean isInvalidEmailMessage(EmailMessage emailMessage) {
+        return Objects.isNull(emailMessage)
+                || Objects.isNull(emailMessage.getTo())
+                || Objects.isNull(emailMessage.getFrom())
+                || Objects.isNull(emailMessage.getSubject())
+                || Objects.isNull(emailMessage.getBody());
     }
 }
